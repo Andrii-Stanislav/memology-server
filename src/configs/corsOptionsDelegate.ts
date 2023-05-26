812cb6189e3937a9cf7f3f3443653f29bdd5fac0
andriiStanislav <a1111@MacBook-Pro-1111.local>
2023-05-26 14:10:15 +0200

    more-edit-cors

--- src/configs/corsOptionsDelegate.ts
+++ src/configs/corsOptionsDelegate.ts
@@ -1,3 +1,7 @@
 export const corsOptions = {
-  origin: [...JSON.parse(process.env.ALLOWED_HOSTS ?? '[]')],
+  // origin: [...JSON.parse(process.env.ALLOWED_HOSTS ?? '[]')],
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
