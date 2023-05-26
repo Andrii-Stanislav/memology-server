7731931d9e9f2ac6386823885d634789de164665
andriiStanislav <a1111@MacBook-Pro-1111.local>
2023-05-26 14:07:05 +0200

    edit-corsOptions

--- src/configs/corsOptionsDelegate.ts
+++ src/configs/corsOptionsDelegate.ts
@@ -1,11 +1,3 @@
-export const corsOptionsDelegate = (req, callback) => {
-  const allowedHosts = JSON.parse(process.env.ALLOWED_HOSTS ?? '[]');
-
-  let corsOptions;
-  if (allowedHosts.includes(req.header('Origin'))) {
-    corsOptions = { origin: true };
-  } else {
-    corsOptions = { origin: false };
-  }
-  callback(null, corsOptions);
+export const corsOptions = {
+  origin: [...JSON.parse(process.env.ALLOWED_HOSTS ?? '[]')],
 };
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { ValidationPipe } from './pipes/validation.pipe';
-import { corsOptionsDelegate, setupSwagger } from './configs';
+import { corsOptions, setupSwagger } from './configs';
 
 async function start() {
   const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
 
   setupSwagger(app);
-  app.enableCors(corsOptionsDelegate);
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
