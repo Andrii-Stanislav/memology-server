f5f8756b14f262fea03d1550c3535deed253e7c6
andriiStanislav <a1111@MacBook-Pro-1111.local>
2023-05-26 14:13:53 +0200

    remove-cors

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { ValidationPipe } from './pipes/validation.pipe';
-import { corsOptions, setupSwagger } from './configs';
+import {
+  // corsOptions,
+  setupSwagger,
+} from './configs';
 
 async function start() {
   const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
 
   setupSwagger(app);
-  app.enableCors(corsOptions);
+  // app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
