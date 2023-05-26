0c10b74c2650cd72dd6ea08419bf7a10dc88c1ca
andriiStanislav <a1111@MacBook-Pro-1111.local>
2023-05-26 13:58:31 +0200

    change-corsOptionsDelegate

--- src/configs/corsOptionsDelegate.ts
+++ src/configs/corsOptionsDelegate.ts
@@ -2,7 +2,7 @@ export const corsOptionsDelegate = (req, callback) => {
   const allowedHosts = JSON.parse(process.env.ALLOWED_HOSTS ?? '[]');
 
   let corsOptions;
-  if (allowedHosts.indexOf(req.header('Origin')) !== -1) {
+  if (allowedHosts.includes(req.header('Origin'))) {
     corsOptions = { origin: true };
   } else {
     corsOptions = { origin: false };
