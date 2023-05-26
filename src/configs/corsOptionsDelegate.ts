8ccd4b6ab44846bc811689a69146bdcc595b926c
andriiStanislav <a1111@MacBook-Pro-1111.local>
2023-05-26 14:36:04 +0200

    gisable-ServeStaticModule

--- src/configs/corsOptionsDelegate.ts
+++ src/configs/corsOptionsDelegate.ts
@@ -1,6 +1,6 @@
 export const corsOptions = {
-  // origin: [...JSON.parse(process.env.ALLOWED_HOSTS ?? '[]')],
-  origin: ['https://memology-game.netlify.app/'],
+  origin: [...JSON.parse(process.env.ALLOWED_HOSTS ?? '[]')],
+  // origin: ['https://memology-game.netlify.app/'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204,
